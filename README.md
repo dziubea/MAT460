a43fcafe6cdd0d05b1defca973a99a37e0fbb39a
dziubea <62551005+dziubea@users.noreply.github.com>
2021-01-15 14:03:03 -0500

    cbc15 link

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # MAT460
 
+Hi there!
 
+A link collection on python, unix/linux, latex, etc. can be found at my webpage:
 [Computational Boot Camp 2015](http://people.sunyit.edu/~dziubea/cbc15/index.html)
---
+++ hello.py
@@ -0,0 +1,12 @@
+print("hi there")
+
+'''
+echo "# MAT460" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/dziubea/MAT460.git
+git push -u origin master
+'''
+#git status
